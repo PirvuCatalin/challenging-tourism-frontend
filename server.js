b583d4b852a8f7d7a1db951468478ff1ff341b6b
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-11-07 12:36:42 +0200

    fix heroku build

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+// add this code
+const whitelist = ['https://test-fast-api-mata.herokuapp.com', 'https://challenging-tourism.herokuapp.com']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
 app.use(cors(corsOptions));
 
 app.use(express.static('./dist/'));
