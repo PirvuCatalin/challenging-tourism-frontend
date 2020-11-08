3103bf43c35394ccfb0176090691ceb8fbf1ecac
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-11-08 05:35:40 +0200

    addded form validation and dashboard info notice

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,29 +13,36 @@ export class LoginComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
     this.form = this.fb.group({
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
   }
+
+  get input() { return this.form.get('email'); }
+  get pass() { return this.form.get('password'); }
 
   ngOnInit() {
     if (this.authService.isLoggedIn()) {
       this.router.navigateByUrl('/');
     }
   }
-
+  hideError = true;
   login() {
-    const val = this.form.value;
+    if (this.form.valid) {
+      this.hideError = true;
+      const val = this.form.value;
 
-    if (val.email && val.password) {
-      this.authService.login(val.email, val.password)
-        .subscribe(
-          () => {
-            console.log("User is logged in");
-            this.router.navigateByUrl('/dashboard');
-          }
-        );
+      if (val.email && val.password) {
+        this.authService.login(val.email, val.password)
+          .subscribe(
+            () => {
+              console.log("User is logged in");
+              this.router.navigateByUrl('/dashboard');
+            }
+          );
+      }
+    } else {
+      this.hideError = false;
     }
   }
-
 }
